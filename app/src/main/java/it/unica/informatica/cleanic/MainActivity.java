fb6e74d46a531f7de8436937bb23d83a390c04c3
Domenico De Luca <domix.deluca@gmail.com>
2021-08-26 02:40:54 +0200

    Change activities to fragments

--- app/src/main/java/it/unica/informatica/cleanic/MainActivity.java
+++ app/src/main/java/it/unica/informatica/cleanic/MainActivity.java
@@ -1,14 +1,47 @@
 package it.unica.informatica.cleanic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView navbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navbar = findViewById(R.id.bottom_navigation);
+
+        navbar.setOnItemSelectedListener(item -> {
+            System.out.println("ciao " + item.getItemId());
+            Fragment selectedFragment;
+            switch(item.getItemId()) {
+                case R.id.home_tab:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.routines_tab:
+                    selectedFragment = new RoutinesFragment();
+                    break;
+                case R.id.remote_tab:
+                    selectedFragment = new RemoteFragment();
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + item.getItemId());
+            }
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.content, selectedFragment);
+            transaction.commit();
+
+            return true;
+        });
     }
 }
---
+++ app/src/main/java/it/unica/informatica/cleanic/RoutinesFragment.java
@@ -0,0 +1,14 @@
+package it.unica.informatica.cleanic;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class RoutinesFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_routines, container, false);
+    }
+}
